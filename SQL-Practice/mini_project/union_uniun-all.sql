ba01548be1babe1e34069e357bd2a0c9a4fea105
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2026-08-06 17:28:12 +0100

    union

---
+++ SQL-Practice/mini_project/union_uniun-all.sql
@@ -0,0 +1,73 @@
+-- ⭐ Exercise 1 (Easy)
+
+-- Return a single list of names from both tables.
+
+-- Columns:
+
+-- person_name
+
+-- Tables:
+
+-- customers
+-- company_employees
+-- ⭐⭐ Exercise 2
+
+-- Create one report showing:
+
+-- person_name
+-- person_type
+
+-- Expected output
+
+-- Alice      Customer
+-- Bob        Customer
+-- John       Employee
+-- Mary       Employee
+
+-- Hint:
+
+-- 'Customer'
+
+-- and
+
+-- 'Employee'
+-- ⭐⭐⭐ Exercise 3
+
+-- Using UNION ALL, return:
+
+-- name
+-- source
+
+-- Expected:
+
+-- Alice      Customer
+-- Bob        Customer
+-- John       Employee
+-- Mary        Employee
+
+-- Sort alphabetically by name.
+
+-- ⭐⭐⭐⭐ Exercise 4 (Challenge)
+
+-- Using the products table, create a report with:
+
+-- category
+-- label
+
+-- First query:
+
+-- Products costing more than ₦100,000
+
+-- Label:
+
+-- Premium
+
+-- Second query:
+
+-- Products costing ₦100,000 or less
+
+-- Label:
+
+-- Standard
+
+-- Combine both with UNION ALL and sort by category.
