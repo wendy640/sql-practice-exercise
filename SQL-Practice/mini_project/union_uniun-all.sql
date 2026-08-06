52c161c758c5d366222b9635e23e953eb44b3fdb
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2026-08-06 18:52:44 +0100

    union_all

--- SQL-Practice/mini_project/union_uniun-all.sql
+++ SQL-Practice/mini_project/union_uniun-all.sql
@@ -10,6 +10,17 @@
 
 -- customers
 -- company_employees
+
+
+SELECT first_name AS person_name
+FROM customers
+
+UNION ALL
+
+SELECT first_name AS person_name
+FROM employees
+
+ORDER BY person_name;
 -- ⭐⭐ Exercise 2
 
 -- Create one report showing:
@@ -71,3 +82,47 @@
 -- Standard
 
 -- Combine both with UNION ALL and sort by category.
+
+-- 🎯 Challenge 2
+
+-- Let's level up.
+
+-- Return two columns:
+
+-- person_name
+-- person_type
+
+select first_name as person_name,
+'Customer' as person_type
+from customers
+union all
+select first_name as person_name,
+'Employees' as person_type
+from employee
+order by person_name
+
+-- Using the products table, write one query that returns:
+
+-- item_name	item_type
+
+-- Requirements:
+
+-- First query:
+-- Products with price > 100000
+-- Label them as 'Premium'
+-- Second query:
+-- Products with price <= 100000
+-- Label them as 'Standard'
+-- Use UNION ALL
+-- Sort by item_name alphabetically.
+select product_name as item_name,
+'Premium' as item_type
+from products
+where price > 100000 
+
+union all
+select product_name as item_name,
+'Standard' as item_type
+from products
+where price <= 100000 
+order by item_name asc
