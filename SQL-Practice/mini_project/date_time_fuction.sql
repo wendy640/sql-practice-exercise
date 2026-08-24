a107deffbd3b031a4ec3650d3fd57b442edcfcb9
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2026-08-24 18:20:53 +0100

    numerical_functionns

--- SQL-Practice/mini_project/date_time_fuction.sql
+++ SQL-Practice/mini_project/date_time_fuction.sql
@@ -51,3 +51,25 @@
 -- and:
 
 -- INTERVAL
+
+select first_name,
+last_name,
+extract( year from hire_date) as hire_year
+from employees
+
+select first_name,last_name,
+extract(month from hire_date) as hire_month
+from employees
+
+select first_name,last_name,
+extract(day from hire_date) as hire_day
+from employees
+
+select first_name,
+last_name,
+age(current_date, hire_date) as duration
+from employees
+
+select *
+from employees
+where hire_date >= current_date - interval '1 year' 
