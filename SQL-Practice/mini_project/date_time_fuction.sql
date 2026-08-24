7dfe0ac29e59ac4ebd9f673931e5de8836963aa7
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2026-08-24 17:34:02 +0100

    numerical_function

---
+++ SQL-Practice/mini_project/date_time_fuction.sql
@@ -0,0 +1,53 @@
+-- Exercise 1
+
+-- Display:
+
+-- employee_name
+-- hire_date
+-- the year each employee was hired.
+
+-- Use EXTRACT().
+
+-- Exercise 2
+
+-- Display:
+
+-- employee_name
+-- hire_date
+-- the month they were hired.
+
+-- Again, use EXTRACT().
+
+-- Exercise 3
+
+-- Display:
+
+-- employee_name
+-- hire_date
+-- the number of days they've worked up to today.
+
+-- Use:
+
+-- CURRENT_DATE
+-- Exercise 4
+
+-- Display:
+
+-- employee_name
+-- hire_date
+-- their employment duration in years/months/days.
+
+-- Use:
+
+-- AGE()
+-- Exercise 5 ⭐
+
+-- Find employees who were hired within the last 1 year.
+
+-- Use:
+
+-- CURRENT_DATE
+
+-- and:
+
+-- INTERVAL
