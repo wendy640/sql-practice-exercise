02444ff918d393128f0c8c1d1fafee9ef9d85b9a
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2026-08-24 17:33:16 +0100

    numerical_functions

---
+++ SQL-Practice/mini_project/numerical_fuctions.sql
@@ -0,0 +1,33 @@
+-- Exercise 1
+
+-- Write a query that displays:
+
+-- employee_name
+-- salary
+-- salary rounded to 2 decimal places
+
+-- Use ROUND().
+
+-- Exercise 2
+
+-- Display:
+
+-- employee_name
+-- salary
+-- salary rounded down
+
+-- Use FLOOR().
+
+-- Exercise 3
+
+-- Display:
+
+-- employee_name
+-- salary
+-- the absolute difference between the employee's salary and 100000
+
+-- Use ABS().
+
+-- Exercise 4
+
+-- Display each employee's employee_id and determine whether the ID is even or odd using MOD().
