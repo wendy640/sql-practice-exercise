207d934f420b07be3813740531823c2b70591040
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2026-09-01 12:57:31 +0100

    nullifs

--- SQL-Practice/mini_project/nullif.sql
+++ SQL-Practice/mini_project/nullif.sql
@@ -10,7 +10,28 @@
 -- but uses NULLIF() to turn a salary of 80,000 into NULL.
 
 
+-- Exercise 2 — NULLIF() with COALESCE()
+
+-- Now let's combine the two functions we've learned.
+
+-- Write a query that returns:
+
+-- employee_name
+-- salary_status
+
+-- If the employee's salary is 80,000, display:
+
+-- Salary not applicable
+
+-- Otherwise, display their actual salary.
+
 select employee_name,
 salary,
-nullif(salary, 80000)
+nullif(salary, 80000) as adjusted_salary
+from company_employees
+
+select employee_name,
+coalesce(
+nullif(salary, 80000)::text,
+'Salary not applicable') as salary_status
 from company_employees
