82367b390d955891640671aec595e2cd52960151
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2026-09-01 13:33:36 +0100

    nullif1

--- SQL-Practice/mini_project/nullif.sql
+++ SQL-Practice/mini_project/nullif.sql
@@ -25,6 +25,12 @@
 
 -- Otherwise, display their actual salary.
 
+-- Let's make this one more practical before moving to GREATEST() and LEAST().
+
+-- Using company_employees, calculate:
+
+-- salary divided by 1000, but if the salary is 0, return NULL instead of attempting the division.
+
 select employee_name,
 salary,
 nullif(salary, 80000) as adjusted_salary
@@ -34,4 +40,8 @@ select employee_name,
 coalesce(
 nullif(salary, 80000)::text,
 'Salary not applicable') as salary_status
+from company_employees
+
+select employee_name,
+salary/nullif(1000,0) as salary_in_thousands
 from company_employees
