f160baa16a9e9604e0026b02b71dba1948c53f43
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2026-09-08 11:56:44 +0100

    subqueries

---
+++ SQL-Practice/mini_project/subqueries.sql
@@ -0,0 +1,21 @@
+-- Exercise 1 — Above Average
+
+-- Find all employees whose salary is greater than the company's average salary.
+
+-- Exercise 2 — Highest Salary
+
+-- Find the employee(s) whose salary is equal to the highest salary in the company.
+
+-- Exercise 3 — Below Average
+
+-- Find all employees whose salary is below the average salary.
+
+-- Exercise 4 — Same Department as Alice
+
+-- Find all employees who work in the same department as Alice.
+
+-- Hint: first find Alice's department_id.
+
+-- Exercise 5 — Higher Than Alice
+
+-- Find all employees whose salary is greater than Alice's salary.
