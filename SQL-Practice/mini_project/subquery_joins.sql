f44e8815669310ee83187a7aaa5bd46488d2d93f
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2026-09-15 11:21:17 +0100

    subquery_join

---
+++ SQL-Practice/mini_project/subquery_joins.sql
@@ -0,0 +1,26 @@
+-- Challenge 1
+
+-- Using company_employees and company_departments:
+
+-- Find all employees who earn more than the average salary of the company, and display their department name.
+
+-- Return:
+
+-- employee_name
+-- department_name
+-- salary
+
+-- You need:
+
+-- JOIN
+-- WHERE
+-- a subquery using AVG()
+-- Challenge 2 🔥
+
+-- Find all employees who earn more than the average salary of the Technical department.
+
+-- Return:
+
+-- employee_name
+-- department_name
+-- salary
