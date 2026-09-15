29fe25c0d3eaac724f0960d3c409b968b8c896fb
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2026-09-15 10:01:09 +0100

    subquery_challenge

--- SQL-Practice/mini_project/exist_and_non_exist.sql
+++ SQL-Practice/mini_project/exist_and_non_exist.sql
@@ -1,1 +1,23 @@
-d
+-- Challenge 1 — EXISTS
+
+-- Find all employees who work in a department where at least one employee earns 85,000 or more.
+
+-- Return:
+
+-- employee_name
+-- department_id
+-- salary
+
+-- Use EXISTS.
+
+-- Challenge 2 — EXISTS
+
+-- Find all employees who work in a department where at least one employee earns less than 70,000.
+
+-- Again, use EXISTS.
+
+-- Challenge 3 — NOT EXISTS
+
+-- Find all employees who work in a department where no employee earns more than 90,000.
+
+-- Use NOT EXISTS.
