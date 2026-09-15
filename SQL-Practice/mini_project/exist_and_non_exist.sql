bb6801fd31f6afd7024f6f90efb772e0b8492dda
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2026-09-15 10:16:58 +0100

    subquery_challenge_answer

--- SQL-Practice/mini_project/exist_and_non_exist.sql
+++ SQL-Practice/mini_project/exist_and_non_exist.sql
@@ -21,3 +21,31 @@
 -- Find all employees who work in a department where no employee earns more than 90,000.
 
 -- Use NOT EXISTS.
+
+select e.employee_name,e.department_id,e.salary
+from company_employees e
+where exists(
+select 1
+from company_employees e2
+where e2.department_id = e.department_id
+and e2.salary >= 85000
+)
+
+select e.employee_name,e.department_id,e.salary
+from company_employees e
+where exists(
+select 1
+from company_employees e2
+where e2.department_id = e.department_id
+and e.salary < 70000
+)
+select e.employee_name,e.department_id,e.salary
+from company_employees e
+where not exists(
+select 1
+from company_employees e2
+where e2.department_id = e.department_id
+and e.salary > 90000
+)
+
+
