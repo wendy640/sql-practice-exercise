a20be930276f77c75df750428dbaef99b617813d
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2026-09-15 12:04:03 +0100

    subquery_joins

--- SQL-Practice/mini_project/subquery_joins.sql
+++ SQL-Practice/mini_project/subquery_joins.sql
@@ -24,3 +24,25 @@
 -- employee_name
 -- department_name
 -- salary
+
+select e.employee_name,d.department_name,e.salary
+from company_employees e
+join company_departments d
+on d.department_id = e.department_id
+where e.salary > (
+select avg(e2.salary)
+from company_employees e2
+)
+SELECT e.employee_name,
+       d.department_name,
+       e.salary
+FROM company_employees e
+JOIN company_departments d
+    ON d.department_id = e.department_id
+WHERE e.salary > (
+    SELECT AVG(e2.salary)
+    FROM company_employees e2
+    JOIN company_departments d2
+        ON d2.department_id = e2.department_id
+    WHERE d2.department_name = 'Technical'
+);
